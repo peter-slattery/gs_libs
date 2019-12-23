38f622104d073f349adf20bbb024d75b03fd84bf
Peter Slattery <slattery.peter.j@gmail.com>
2019-12-23 09:10:44 -0800

    More updates to gs_memory_arena - temporary memory rewinding and a few bug fixes

--- src/gs_memory_arena.h
+++ src/gs_memory_arena.h
@@ -164,8 +164,6 @@ if(!(expression)) { \
 #define GSMem_Assert(expression)
 #endif
 
-
-
 enum gs_memory_expansion_rule
 {
     MemoryExpansion_Allowed, // Zero is initialization lets the memory grow on its own
@@ -174,8 +172,26 @@ enum gs_memory_expansion_rule
     MemoryExpansion_Count,
 };
 
+// NOTE(Peter):
+// This rule is only here to allow for taking arena snapshots. The problem this solves
+// is if you take a snapshot while there are 'holes' in memory_buffers behind the 
+// most recently added memory_buffer, take a snapshot of that arena, then push something
+// on that fits in one of those holes, we will fill the hole and be unable to track/free
+// that addition via the snapshot construct. 
+//
+// By requiring that allocations in a buffer only come from the most recent memory_buffer
+// we can very easily rewind the buffer to the correct location. 
+// Hence FindAddress_InLastBufferOnly
+enum gs_memory_find_address_rule
+{
+    FindAddress_InAnyBuffer,
+    FindAddress_InLastBufferOnly,
+    FindAddress_Count,
+};
+
 typedef void* gs_memory_alloc(gs_mem_u32 Size);
-typedef void* gs_memory_realloc(void* Address, gs_mem_u32 Size);
+typedef void* gs_memory_realloc(void* Address, gs_mem_u32 OldSize, gs_mem_u32 NewSize);
+typedef void gs_memory_free(void* Address, gs_mem_u32 Size);
 
 #ifndef GS_MEMORY_BUFFER_SIZE
 #define GS_MEMORY_BUFFER_SIZE 1024
@@ -216,11 +232,12 @@ struct memory_arena
     gs_mem_u32 TotalUsed;
     gs_mem_u32 TotalSize;
     
+    gs_memory_find_address_rule FindAddressRule;
     gs_memory_expansion_rule ExpansionRule;
     gs_memory_alloc* Alloc;
     gs_memory_realloc* Realloc;
     
-    #ifdef GS_MEMORY_TRACK_ALLOCATIONS
+#ifdef GS_MEMORY_TRACK_ALLOCATIONS
      tracked_allocation_buffer** AllocationBuffers;
     gs_mem_u32 AllocationBuffersCount;
     gs_mem_u32 AllocationsUsed;
@@ -233,6 +250,45 @@ struct address_and_buffer
      gs_mem_u64 Address;
     gs_mem_u32 SizeWithAlignment;
 };
+
+struct arena_snapshot
+{
+    gs_mem_u32 ArenaUsedAtSnapshot;
+    gs_mem_u32 HeadBufferUsedAtSnapshot;
+    gs_mem_u32 HeadBufferAtSnapshot;
+    memory_arena* Arena;
+    
+#ifdef GS_MEMORY_TRACK_ALLOCATIONS
+    gs_mem_u32 AllocationsUsedAtSnapshot;
+    #endif
+};
+
+static void
+FreeMemoryArena(memory_arena* Arena, gs_memory_free* Free)
+{
+    if (Free)
+    {
+for (gs_mem_u32 i = 0; i < Arena->BuffersCount; i++)
+    {
+        memory_buffer* Buffer = Arena->Buffers + i;
+        Free(Buffer->Buffer, Buffer->Size);
+    }
+        Free(Arena->Buffers, sizeof(memory_buffer) * Arena->BuffersCount);
+    }
+    else
+    {
+#ifdef GS_MEMORY_NO_STD_LIBS
+        GSMem_Assert(0);
+#else
+        for (gs_mem_u32 i = 0; i < Arena->BuffersCount; i++)
+    {
+        memory_buffer* Buffer = Arena->Buffers + i;
+        free(Buffer->Buffer);
+    }
+        free(Arena->Buffers);
+        #endif
+    }
+}
 
 #define IsPowerOfTwo(v) ((v != 0) && ((v & (v - 1)) == 0))
 
@@ -309,13 +365,13 @@ if (Arena->Alloc)
 }
 
 static gs_mem_u8*
-ArenaRealloc(memory_arena* Arena, gs_mem_u8* Head, gs_mem_u32 Size)
+ArenaRealloc(memory_arena* Arena, gs_mem_u8* Head, gs_mem_u32 OldSize, gs_mem_u32 NewSize)
 {
     gs_mem_u8* Result = 0;
     
     if (Arena->Realloc != 0)
     {
-        Result = (gs_mem_u8*)Arena->Realloc(Head, Size);
+        Result = (gs_mem_u8*)Arena->Realloc(Head, OldSize, NewSize);
     }
     else
     {
@@ -324,7 +380,7 @@ ArenaRealloc(memory_arena* Arena, gs_mem_u8* Head, gs_mem_u32 Size)
         // we should assert as this is an invalid codepath
         GSMem_Assert(0);
         #else
-         Result = (gs_mem_u8*)realloc(Head, Size);
+         Result = (gs_mem_u8*)realloc(Head, NewSize);
         #endif
     }
 
@@ -335,12 +391,15 @@ static memory_buffer*
 GrowArena(memory_arena* Arena, gs_mem_u32 SizeNeeded)
 {
     GSMem_Assert(Arena->ExpansionRule != MemoryExpansion_Disallowed);
-    GSMem_Assert(!(Arena->ExpansionRule == MemoryExpansion_OnlyIfFunctionsProvided 
-             && (Arena->Alloc == 0 || Arena->Realloc == 0)));
+    if (Arena->ExpansionRule == MemoryExpansion_OnlyIfFunctionsProvided)
+    {
+        GSMem_Assert((Arena->Alloc != 0) && (Arena->Realloc != 0));
+    }
     
-gs_mem_u32 NewBuffersCount = (Arena->BuffersCount + 1);
+    gs_mem_u32 NewBuffersCount = (Arena->BuffersCount + 1);
+    gs_mem_u32 OldBuffersSize = sizeof(memory_buffer) * Arena->BuffersCount;
     gs_mem_u32 NewBuffersSize = sizeof(memory_buffer) * NewBuffersCount;
-    Arena->Buffers = (memory_buffer*)ArenaRealloc(Arena, (gs_mem_u8*)Arena->Buffers, NewBuffersSize);
+    Arena->Buffers = (memory_buffer*)ArenaRealloc(Arena, (gs_mem_u8*)Arena->Buffers, OldBuffersSize, NewBuffersSize);
     Arena->BuffersCount = NewBuffersCount;
     
     memory_buffer* NewBuffer = Arena->Buffers + (Arena->BuffersCount - 1);
@@ -361,16 +420,19 @@ gs_mem_u32 NewBuffersCount = (Arena->BuffersCount + 1);
 
 #define DetermineAllocationSize(size) (size) + GS_MEMORY_FOOTER_SIZE
 #define ClearAllocationsUsed(arena) (arena)->AllocationsUsed = 0
-    
+#define ClearAllocationsUsedToSnapshot(arena, snapshot) \
+(arena)->AllocationsUsed = (snapshot).AllocationsUsedAtSnapshot;
+
 static void
 TrackAllocation(memory_arena* Arena, gs_mem_u8* Head, gs_mem_u32 Size, char* Filename, gs_mem_u32 LineNumber)
 {
-    gs_mem_u32 AllocationsMax = Arena->AllocationBuffersCount * GS_MEM_TRACKED_ALLOCATION_BUFFER_SIZE;
+gs_mem_u32 AllocationsMax = Arena->AllocationBuffersCount * GS_MEM_TRACKED_ALLOCATION_BUFFER_SIZE;
     if (Arena->AllocationsUsed >= AllocationsMax)
     {
         gs_mem_u32 NewAllocationBuffersCount = Arena->AllocationBuffersCount + 1;
         Arena->AllocationBuffers = (tracked_allocation_buffer**)ArenaRealloc(Arena, 
-(gs_mem_u8*)Arena->AllocationBuffers, 
+                                                                             (gs_mem_u8*)Arena->AllocationBuffers, 
+                                                                             Arena->AllocationBuffersCount * sizeof(void*),
 NewAllocationBuffersCount * sizeof(void*));
         Arena->AllocationBuffersCount = NewAllocationBuffersCount;
         
@@ -435,6 +497,8 @@ GSMem_Assert(VerifyAllocationNoOverflow(Allocation));
 #define AssertAllocationsNoOverflow(arena)
 #define DetermineAllocationSize(size) size
 #define ClearAllocationsUsed(arena)
+#define ClearAllocationsUsedToSnapshot(arena, snapshot)
+
 #define TrackAllocation(arena, head, size, filename, linenumber) 
 
 #define PushSize(arena, size) PushSize_((arena), (size))
@@ -449,15 +513,26 @@ GSMem_Assert(VerifyAllocationNoOverflow(Allocation));
 static gs_mem_u8*
 PushSize_(memory_arena* Arena, gs_mem_u32 Size, gs_mem_u32 Alignment = 4, char* Filename = 0, gs_mem_u32 LineNumber = 0)
 {
-GSMem_Assert(IsPowerOfTwo(Alignment));
     // ie. Alignment = 4 = 100 (binary)
     // 4 - 1 = 3 
     // 100 - 1 = 011 which is a mask of the bits we don't want set in the start address
-    gs_mem_u32 AlignmentMask = Alignment - 1;
+    GSMem_Assert(IsPowerOfTwo(Alignment));
+gs_mem_u32 AlignmentMask = Alignment - 1;
     
     gs_mem_u32 AllocationSize = DetermineAllocationSize(Size);
     
-address_and_buffer ResultAddress = FindAlignedAddressInBufferWithRoom(Arena, AllocationSize, Alignment, AlignmentMask);
+    address_and_buffer ResultAddress = {};
+    if (Arena->FindAddressRule == FindAddress_InAnyBuffer)
+    {
+        ResultAddress = FindAlignedAddressInBufferWithRoom(Arena, AllocationSize, Alignment, AlignmentMask);
+    }
+    else if (Arena->FindAddressRule == FindAddress_InLastBufferOnly
+             && Arena->BuffersCount > 0)
+    {
+        memory_buffer* LastBuffer = Arena->Buffers + Arena->BuffersCount - 1;
+        ResultAddress = GetAlignedAddressInBuffer(LastBuffer, Size, Alignment, AlignmentMask);
+    }
+
     if (ResultAddress.Address == 0)
     {
         memory_buffer* Buffer = GrowArena(Arena, AllocationSize);
@@ -488,5 +563,51 @@ ClearArena(memory_arena* Arena)
     ClearAllocationsUsed(Arena);
 }
 
+static arena_snapshot
+TakeSnapshotOfArena(memory_arena* Arena)
+{
+    Assert(Arena->FindAddressRule == FindAddress_InLastBufferOnly);
+
+arena_snapshot Result = {};
+    Result.Arena = Arena;
+    Result.ArenaUsedAtSnapshot = Arena->TotalUsed;
+    if (Arena->BuffersCount > 0)
+    {
+        Result.HeadBufferAtSnapshot = Arena->BuffersCount - 1;
+    }
+    else
+    {
+        Result.HeadBufferAtSnapshot = 0;
+    }
+
+    memory_buffer* HeadBuffer = Arena->Buffers + Result.HeadBufferAtSnapshot;
+    if (HeadBuffer)
+    {
+        Result.HeadBufferUsedAtSnapshot = HeadBuffer->Used;
+    }
+    
+return Result;
+}
+
+static void
+ClearArenaToSnapshot(memory_arena* Arena, arena_snapshot Snapshot)
+{
+    Assert(Arena == Snapshot.Arena);
+    
+    memory_buffer* HeadBufferAtSnapshot = Arena->Buffers + Snapshot.HeadBufferAtSnapshot;
+    if (HeadBufferAtSnapshot)
+    {
+    HeadBufferAtSnapshot->Used = Snapshot.HeadBufferUsedAtSnapshot;
+    
+    for (gs_mem_u32 i = Snapshot.HeadBufferAtSnapshot + 1; i < Arena->BuffersCount; i++)
+    {
+        memory_buffer* Buffer = Arena->Buffers + i;
+        Buffer->Used = 0;
+    }
+    }
+    
+    Arena->TotalUsed = Snapshot.ArenaUsedAtSnapshot;
+    ClearAllocationsUsedToSnapshot(Arena, Snapshot);
+}
 #define GS_MEMORY_ARENA_H
 #endif // GS_MEMORY_ARENA_H
